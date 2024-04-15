5db6fc3db0cd23e565fdcfa789be8d78a9ad7dd6
Jakub Smilowski <257043@vut.cz>
2024-04-15 11:54:43 +0200

    vr01

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -8,10 +8,26 @@ def read_data(file_name):
     :param file_name: (str), name of CSV file
     :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
     """
+    dtst = {}
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r",) as file:
+
+        tmp_i = csv.DictReader(file)
+
+        dtst = {}
+
+        for row in tmp_i:
+            for hdr, val in row.items():
+                if hdr not in dtst:
+                    dtst[hdr] = [int(val)]
+                else:
+                    dtst[hdr].append(int(val))
 
 
+    return dtst
+tst = read_data("numbers.csv")
+print(tst)
 def main():
     pass
 
