4bb6e5b93d7c8866bae811312b15c61a0e9d6840
Jakub Smilowski <257043@vut.cz>
2025-04-17 12:47:17 +0200

    přidán bubble a insertion sort

--- sorting.py
+++ sorting.py
@@ -51,12 +51,52 @@ def selection_sort(numlist,direc= 0):
     return numlist
 
 
+def  bubble_sort(numlist,direct=0):
+
+    for i in range(len(numlist)-1):
+        for j in range(len(numlist)-i-1):
+            if direct == 0:
+                if numlist[j] > numlist[j+1]:
+                    numlist[j], numlist[j+1] = numlist[j+1], numlist[j]
+            else:
+                if numlist[j] < numlist[j + 1]:
+                    numlist[j], numlist[j + 1] = numlist[j + 1], numlist[j]
+
+
+    return numlist
+
+
+def insertion_sort(numlist):
+
+    for i in range(1,len(numlist)):
+        j = i - 1
+        temp = numlist[i]
+        while j >= 0 and temp < numlist[j]:
+            numlist[j+1] = numlist[j]
+            j -= 1
+        numlist[j+1] = temp
+
+    return numlist
+
+
 
 def main():
     data = read_data("numbers.csv")
 
+    print("Selection sort")
     for i in data.keys():
-        print(selection_sort(data[i],1))
+        print(f"vzestupně:{i} - {selection_sort(data[i], 0)}")
+        print(f"sestupně:{i} - {selection_sort(data[i], 1)}")
+
+    print("\nBubble sort")
+    for k in data.keys():
+        print(f"vzestupně:{k} - {bubble_sort(data[k])}")
+        print(f"sestupně:{k} - {bubble_sort(data[k], 1)}")
+
+    print("\nInsertion sort")
+    for m in data.keys():
+        print(f"vzestupně:{m} - {insertion_sort(data[m])}")
+
 
 if __name__ == '__main__':
     main()
